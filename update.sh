83d7e3750c597fe1080233ee089f8dd257c0c670
tneele <tneele@student.scalda.nl>
2021-04-19 12:40:29 +0200

    made an update script and updated autostart

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ git pull
 echo copy autostart to lxsession...
 sudo cp ~/slidehub/autostart /etc/xdg/lxsession/LXDE-pi/autostart
 sudo cp ~/slidehub/autostart /etc/xdg/lxsession/LXDE/autostart
+echo done!
